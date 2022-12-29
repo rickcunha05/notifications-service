f091ed5997c7461501b1881427913eb7f0fed821
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2022-12-29 18:45:50 -0300

    implementation Factory Pattern in notification!

--- src/application/use-cases/count-recipient-notification.spec.ts
+++ src/application/use-cases/count-recipient-notification.spec.ts
@@ -1,7 +1,6 @@
 import { CountRecipientNotification } from './count-recipient-notifications';
-import { Content } from '@application/entities/content';
-import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Count recipients notifications', () => {
     it('should be able to count recipient a notification', async () => {
@@ -9,24 +8,19 @@ describe('Count recipients notifications', () => {
         const countRecipientNotifications = new CountRecipientNotification(notificationsRepository);
 
         await notificationsRepository.create(
-            new Notification({
-                content: new Content('This is a notification'),
-                category: 'Social',
-                recipientId: 'recipient-1'
-            })
+            makeNotification({ recipientId: 'recipient-1' })
         )
         await notificationsRepository.create(
-            new Notification({
-                content: new Content('This is a notification'),
-                category: 'Social',
-                recipientId: 'recipient-2'
-            })
+            makeNotification({ recipientId: 'recipient-1' })
+        )
+        await notificationsRepository.create(
+            makeNotification({ recipientId: 'recipient-2' })
         )
 
         const { count } = await countRecipientNotifications.execute({
             recipientId: 'recipient-1'
         })
 
-        expect(count)
+        expect(count).toEqual(2);
     })
 })
